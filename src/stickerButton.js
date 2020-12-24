ef3ea58e447db1028f27cea71ad11f26bb5fa6ea
Marco Spinello <marco@eclecticiq.com>
2020-12-24 13:54:12 +0100

    fix more stuff

--- src/stickerButton.js
+++ src/stickerButton.js
@@ -1,16 +1,13 @@
-const buttonIcon = '<rect x="0" y="0" width="24" height="24" fill="currentColor" />',
-
-const today = new Date().toDateString(),
-
 miro.onReady(() => {
   miro.initializeInner({
     extensionPoints: {
       bottomBar: {
         title: "add sticker with today date",
-        svgIcon: buttonIcon
-        onClick: function () {
+        svgIcon: '<rect x="0" y="0" width="24" height="24" fill="currentColor" />',
+        onClick: () => {
+          let todaysDate = new Date().toDateString()
           miro.board.widgets.create({type:'sticker', text: 'Today is', x: 0, y: 0})
-          miro.board.widgets.update({id: sticker.id, text: today, style:{stickerBackgroundColor:'#7ac673'}})
+          miro.board.widgets.update({id: sticker.id, text: todaysDate, style:{stickerBackgroundColor:'#7ac673'}})
         },
       },
     },
