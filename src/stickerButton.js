781a6e1651b746a25e9eb4f9c02c293a198661ae
Marco Spinello <marco@eclecticiq.com>
2020-12-27 16:01:15 +0100

    debug

--- src/stickerButton.js
+++ src/stickerButton.js
@@ -4,14 +4,14 @@ miro.onReady(() => {
   miro.initializeInner({
     extensionPoints: {
       bottomBar: {
-        title: "add sticker with today\'s date date",
+        title: "add a sticker with today\'s date",
         svgIcon: blackButton,
         onClick: () => {
-          const waitOneSec = () => new Promise( r => setTimeout( r, 2000 ) )
+          const waitOneSec = () => new Promise(r => setTimeout(r, 2000))
           let todaysDate = new Date().toDateString()
-          let sticker = ( await miro.board.widgets.create( { type:'sticker', text: 'Today is', x: 0, y: 0 } ) )[0]
+          let sticker = (await miro.board.widgets.create({type: 'sticker', text: 'Today is'}))[0]
           await waitOneSec()
-          await miro.board.widgets.update( { id: sticker.id, text: todaysDate, style:{ stickerBackgroundColor:'#7ac673' } } )
+          await miro.board.widgets.update({id: sticker.id, text: todaysDate, style:{stickerBackgroundColor:'#7ac673'}})
         },
       },
     },
